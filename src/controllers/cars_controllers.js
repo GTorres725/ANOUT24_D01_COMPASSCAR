d889845bc4278f29b1802e7c2b833c0b6c8d6ab3
Luiz Guilherme <luiz.camargo.pb@compasso.com.br>
2024-11-05 16:58:10 -0300

    feat(app, routes, controllers): add cars routes and controllers setup
    - Modify app.js to include cars_routes
    
    - Create routes and controllers folders in src
    
    - Add cars_routes.js with base route structure
    
    - Add cars_controllers.js with basic controller setup

---
+++ src/controllers/cars_controllers.js
@@ -0,0 +1,25 @@
+const knex = require ('knex');
+
+exports.add_car = async (req, res)=> {
+
+};
+
+exports.get_allCar = async (req, res)=> {
+
+};
+
+exports.get_car = async (req, res)=> {
+
+};
+
+exports.update_put_car = async (req, res)=> {
+
+};
+
+exports.update_patch_car = async (req, res)=> {
+
+};
+
+exports.del_car = async (req, res)=> {
+
+};
