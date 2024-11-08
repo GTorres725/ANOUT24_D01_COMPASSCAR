7cdb52cfcd4696c39b8af783815b5055166185fa
Luiz Guilherme <luiz.camargo.pb@compasso.com.br>
2024-11-08 09:07:19 -0300

    feat(controllers): configures the PATCH route of the update_patch_car function in cars_controllers.js

--- src/controllers/cars_controllers.js
+++ src/controllers/cars_controllers.js
@@ -7,11 +7,15 @@ exports.add_car = async (req, res)=> {
     if(!year) res.status(400).json({ error: 'year is required' });
     if(!plate) res.status(400).json({ error: 'plate is required' });
 
-    let currentYear = new Date().getFullYear();
-    if(+year < (currentYear -9) || +year > (currentYear +1)) {
-        res.status(400).json({ error: `year must be between ${currentYear -9} and ${currentYear +1}` });
-    };
-    let created_at = new Date()
+    yearVerification = ()=> {
+        let currentYear = new Date().getFullYear();
+        if(isNaN(+year)) return res.status(400).json({ error: `year must be between ${currentYear -9} and ${currentYear +1}` })
+        if(+year < (currentYear -9) || +year > (currentYear +1)) {
+            return res.status(400).json({ error: `year must be between ${currentYear -9} and ${currentYear +1}` });
+        };
+    }
+
+    yearVerification();
 
     checkPlate = ()=> {
         if(plate.length == 8) {
@@ -80,7 +84,7 @@ exports.get_allCar = async (req, res)=> {
     const { year, final_plate, brand, page, limit} = req.params;
 
     try {
-        let getCars = kenx('cars');
+        let getCars = db('cars');
 
         if(year) getCars = getCars.where('year', '>=', year);
         
@@ -88,7 +92,7 @@ exports.get_allCar = async (req, res)=> {
 
         if(brand) getCars = getCars.where('brand', 'LIKE', `%${brand}`);
 
-        const cars = await getCars.select()
+        const cars = await getCars.select();
 
         res.status(200).json({cars})
     } catch (error) {
@@ -105,7 +109,7 @@ exports.get_car = async (req, res)=> {
     if(carVerification.length == 0) return res.status(404).json({ error: 'car not found' });
 
     const car = await db('cars').select('*').where('id', '=', id);
-    const car_items = await db('cars_items').select('name').where('car_id', '=', id)
+    const car_items = await db('cars_items').select('name').where('car_id', '=', id);
     const itemsName = car_items.map((i) => {
         return i.name;
     })
@@ -122,7 +126,7 @@ exports.update_put_car = async (req, res)=> {
     const items = req.body;
 
     const idCar = await db('cars').where('id', '=', id)
-    if(idCar.length == 0) return res.status(404).json({ error: 'car not found' })
+    if(idCar.length == 0) return res.status(404).json({ error: 'car not found' });
 
     if(items.length == 0) return res.status(400).json({ error: 'items is required' });
 
@@ -146,9 +150,94 @@ exports.update_put_car = async (req, res)=> {
     }
 };
 
-exports.update_patch_car = async (req, res)=> {
+//
 
+exports.update_patch_car = async (req, res)=> {
+    const { id } = req.params;
+    let { brand, model, year, plate } = req.body;
+
+    const idCar = await db('cars').where('id', '=', id)
+    if(idCar.length == 0) return res.status(404).json({ error: 'car not found' });
+
+    if(brand == '') brand = undefined;
+    if(year == '') year = undefined;
+    if(plate == '') plate = undefined;
+
+    if(plate) {
+        const duplicateCar = await db('cars').where({ plate });
+        if(duplicateCar.length > 0) return res.status(409).json({ error: 'car already registered' });
+    }
+
+    if(brand && !model) return res.status(400).json({ error: 'model must also be informed' })
+
+    yearVerification = ()=> {
+        let currentYear = new Date().getFullYear();
+        if(isNaN(+year)) return res.status(400).json({ error: `year must be between ${currentYear -9} and ${currentYear +1}` })
+        if(+year < (currentYear -9) || +year > (currentYear +1)) {
+            return res.status(400).json({ error: `year must be between ${currentYear -9} and ${currentYear +1}` });
+        };
+    }
+    if(year != undefined) yearVerification();
+
+    checkPlate = ()=> {
+        if(plate.length == 8) {
+             const firstThreeChar = plate.slice(0, 3).split('');//AZ
+             const fourthChar = plate[3]; //-
+             const fifthChar = plate[4]; //09
+             const sixthChar = plate[5]; //AJ09
+             const lastTwoChar = plate.slice(-2).split(''); //09
+             lastTwoChar.push(fifthChar);
+     
+             try {
+                 firstThreeChar.forEach(i => {
+                     if (i >= 'A' && i <= 'Z') {
+                        i++
+                     } else {
+                         throw Error
+                     }
+                 });
+             } catch {
+                return res.status(400).json({ error: 'plate must be in the correct format ABC-1C34' })
+             }
+             
+             if (fourthChar != '-') {
+                return res.status(400).json({ error: 'plate must be in the correct format ABC-1C34' })
+             }
+     
+             if((sixthChar >= '0' && sixthChar <= '9') || (sixthChar >= 'A' && sixthChar <= 'J')) {
+             } else {
+                return res.status(400).json({ error: 'plate must be in the correct format ABC-1C34' })
+             }
+     
+             try {
+                 lastTwoChar.forEach(i => {
+                     if(i >= 0 && i <= 9){
+                         i++;
+                     } else {
+                         throw Error;
+                     }
+                 })
+             } catch {
+               return res.status(400).json({ error: 'plate must be in the correct format ABC-1C34' });
+             }
+             
+        } else {
+            res.status(400).json({ error: 'plate must be in the correct format ABC-1C34' })
+        } 
+     }
+     if(plate != undefined) checkPlate();
+
+     try {
+        await db('cars').update({ brand, model, year, plate }).where({ id });
+        res.status(204).send()
+     } catch (error) {
+        console.log(error);
+        
+        res.status(500).json({ error: 'Internal error' })
+     }
 };
+
+//
 
 exports.del_car = async (req, res)=> {
 
